4b90d9a9580fae54d4f7100d56a095c0e90c34d4
jasdipsekhon <jasdipsekhon@gmail.com>
2020-02-02 23:29:58 -0800

    added comments

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 //
 //  main.c
-//  unixShell2
+//  unixShell5
 //
-//  Created by Jasdip Sekhon on 3/22/19.
+//  Created by Jasdip Sekhon on 3/23/19.
 //  Copyright © 2019 Jasdip Sekhon. All rights reserved.
 //
 
@@ -14,55 +14,64 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #define DELIMS "\t\r\n"
-#define MAX 1024
+#define MAX 2048
 
-/*static char* skipWhiteSpace(char* c)
-{
-    while (isspace(*c)) ++c;
-    return c;
-}
-*/
 int main(int argc, char *argv[])
-{
+{  
     char *cmd;
     char cmd_line[MAX];
-    
-    while (1) {
-        //printf("To exit type 'exit' \n");
+    while (1)
+    {             
         printf("$$ ");
         
+        
+        // wait to read
+        // if no stream of characters read, break
+        //char* fgets (char* str, int num, FILE* stream );
+        // makes it so '$$' is not repeatedly printed
         if (!fgets(cmd_line, MAX, stdin))
+        {
             break;
+        }
+        
+        
+        // points to string that represents program name; invoked by execvp
         argv[0] = cmd_line;
         
-        if ((cmd = strtok(cmd_line, DELIMS))) {
-            //cmd = skipWhiteSpace(cmd);
+
+        // read
+        // read command
+        // breaks string str into a series of tokens using the delimiter delim.
+        if ((cmd = strtok(cmd_line, DELIMS)))
+        {
+    
+            //if user types exit, end program
             if (strcmp(cmd, "exit") == 0)
             {
+                //indicates successful program termination
                 exit(0);
                 break;
             }
-            if(fork() == 0)
+                  
+            
+            //child process
+            //fork() returns -1 if it fails, and if it succeeds, it returns the forked child's pid in the parent, and 0 in the child
+            if (fork() == 0)
             {
-                if (strcmp(cmd, "cd") == 0)
-                {
-                    char *arg = strtok(0, DELIMS);
-                    if (!arg)
-                        fprintf(stderr, "argument missing \n");
-                    else
-                        chdir(arg);
-                }
-                else
-                {
-                    execvp(argv[0],argv);
-                }
+                //execute child
+                //replaces the current running process with a new process
+                execvp(argv[0], argv);
+                //indicates successful program termination
                 exit(0);
             }
+            //parent process
             else
             {
+                //wait until child completes its execution
                 wait(0);
-            }
+            }        
         }
     }
     return 0;
 }
+
